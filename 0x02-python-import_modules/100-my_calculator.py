9e02a085b89079c70dbf3f044dbc5b1cb1560f7e
dsimwinga <infor.dsimwinga@gmail.com>
2023-11-06 09:13:26 +0200

    100-my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 from sys import argv
+from calculator_1 import add, sub, mul, div
+if __name__ != "__main__":
+    exit()
 
-if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Usage:", argv[0], "<a> <operator> <b>")
-        exit(1)
+argc = len(argv) - 1
+if argc != 3:
+    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+    exit(1)
+elif argv[2] == '+':
+    func = add
+elif argv[2] == '-':
+    func = sub
+elif argv[2] == '*':
+    func = mul
+elif argv[2] == '/':
+    func = div
+else:
+    print("Unknown operator. Available operators: +, -, *, and /")
+    exit(1)
 
-    op = argv[2]
-    f = {"+": add, "-": sub, "*": mul, "/": div}
-    if op not in f:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
-    a = int(argv[1])
-    b = int(argv[3])
-    print("{:d} {:s} {:d} = {:d}".format(a, op, b, f[op](a, b)))
+result = func(int(argv[1]), int(argv[3]))
+print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
+    argv[2], int(argv[3]), result))
